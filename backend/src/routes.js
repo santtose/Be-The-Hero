25d6cf60cb4c29f2dacf59cec68b205b1bfd9b7b
santtose <santtose@outlook.com>
2020-06-05 16:19:17 -0300

    Projeto Be The Hero

---
+++ backend/src/routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+//Configurando a rota
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros enviados na rota após '?' (filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisiçãon
+ */
+
+ /**
+  * Banco de dados
+  * Driver: SELECT * FROM users
+  * Query Builder: table('users').select ('*').where()
+  */
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+module.exports = routes;
